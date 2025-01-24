9f39b5c0d27ab3fcf0b3f8ac2b5602ee9e703808
Fernando Gonzalez <fer.dg.sc@gmail.com>
2025-01-23 23:54:57 -0300

    Se crea función agregarAmigo() y limpiarInput()

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+// Función para limpiar el input después de agregar un amigo al array.
+function limpiarInput() {
+    document.getElementById("amigo").value = "";
+}
+
+// Función para agregar un amigo a la lista desde el input.
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value.trim(); // trim() elimina los espacios en blanco al inicio y al final del string.
+    // Verifica si se agrega un nombre no válido.
+    if(nombre !== "") {
+        amigos.push(nombre);
+        console.log(amigos);
+    } else {
+        alert("Por favor, ingrese un nombre válido.");
+    }
+    limpiarInput();
+}
