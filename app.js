201c79a969d8e44e78f556b1d34c1a19ef3c1c89
Fernando Gonzalez <fer.dg.sc@gmail.com>
2025-01-24 15:23:17 -0300

    Se agregó la función para reiniciar el sorteo.

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ let amigos = [];
 
 // Agrega un nombre al array desde el input, haciendo clic en el botón añadir.
 function agregarAmigo() {
+    let resultado = document.getElementById("resultado");
+    if(resultado.innerHTML != "") {
+        reiniciarSorteo();
+    }
+
     let nombre = document.getElementById("amigo").value.trim(); // trim() elimina los espacios en blanco al inicio y al final del string.
     // Verifica si se agrega un nombre no válido.
     if(nombre !== "") {
-        amigos.push(nombre);        
+        amigos.push(nombre);               
     } else {
         alert("Por favor, ingrese un nombre válido.");
     }
     limpiarInput();
-    actualizarLista();
+    actualizarLista();    
 }
 
 // Limpia el input después de hacer clic en añadir.
@@ -43,9 +48,21 @@ function sortearAmigo() {
         let amigoSecreto = amigos[indice];
         // Muestra el nombre sorteado en el HTML.
         let resultado = document.getElementById("resultado");
-        resultado.innerHTML = `El amigo secreto sorteado es: ${amigoSecreto}`;
+        resultado.innerHTML = `El amigo secreto sorteado es: ${amigoSecreto}`;        
 
         let lista = document.getElementById("listaAmigos");
-        lista.innerHTML = "";  // Limpia la lista de amigos para que no se vean los nombres, solamente el amigo secreto sorteado.
+        lista.innerHTML = "";  // Limpia la lista de amigos en el DOM pero no modifica el array.        
     }
+}
+
+// Reinicia el sorteo, como si se recargara la página al ingresar un nuevo nombre en el input.
+function reiniciarSorteo() {
+    // Lipia el array amigos.
+    amigos = [];    
+    // Limpia la lista de amigos en el DOM.
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    // Eliminar el mensaje de amigo secreto sorteado.
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
 }
