d00fc2ef0ba3f504a18fa2c828600826eb18d9a8
Fernando Gonzalez <fer.dg.sc@gmail.com>
2025-01-24 10:20:13 -0300

    Se agrega la función actualizarLista() y comentarios.

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 let amigos = [];
 
-// Función para limpiar el input después de agregar un amigo al array.
-function limpiarInput() {
-    document.getElementById("amigo").value = "";
-}
-
-// Función para agregar un amigo a la lista desde el input.
+// Agrega un nombre al array desde el input, haciendo clic en el botón añadir.
 function agregarAmigo() {
     let nombre = document.getElementById("amigo").value.trim(); // trim() elimina los espacios en blanco al inicio y al final del string.
     // Verifica si se agrega un nombre no válido.
@@ -16,4 +11,24 @@ function agregarAmigo() {
         alert("Por favor, ingrese un nombre válido.");
     }
     limpiarInput();
+    actualizarLista();
+}
+
+// Limpia el input después de hacer clic en añadir.
+function limpiarInput() {
+    document.getElementById("amigo").value = "";    
+}
+
+// Agrega los nombres como un elemento <li>.
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");    
+    lista.innerHTML = "";  // Limpia la lista antes de agregar los nombres para que no se dupliquen constantemente.
+
+    let i = 0;
+    while(i < amigos.length) {
+        let li = document.createElement("li");  // El elemento <li> se crea en memoria.
+        li.textContent = amigos[i]
+        lista.appendChild(li);  // appendChild() lo inserta en el DOM, haciéndolo visible en la web.
+        i++;
+    }
 }
